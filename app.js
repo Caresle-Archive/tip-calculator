ccf60502b4108815bfda352a03916eecca46cb8a
Caresle <carabesle@gmail.com>
2021-08-28 20:23:38 -0600

    Add tip calcs

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const inputBill = document.getElementById('inputBill')
 const inputPerson = document.getElementById('inputPerson')
-const valueCard = document.getElementById('value-card')
+const inputBtn = document.getElementById('btn-custom')
+const inputs = document.querySelectorAll('input')
+
+const tipTotalPerson = document.getElementById('tip-total-text')
+const tipPerson = document.getElementById('tip-person-text')
+
 const resetButton = document.getElementById('reset-button')
 const buttonsBill = document.querySelectorAll('.btn-bill')
 
@@ -13,11 +18,6 @@ const addBtnStyle = (btn, addS, remS) => {
 
 // Add the style before click the button
 const removeBtnActive = (btn, remS, addS) => {
-	if (btn.textContent === 'Custom') {
-		addBtnStyle(btn, 'btn-gray', 'btn-primary-outline')
-		return
-	}
-
 	if (btn !== '') {
 		addBtnStyle(btn, addS, remS)
 	}
@@ -25,14 +25,48 @@ const removeBtnActive = (btn, remS, addS) => {
 
 const clickBillButton = (e) => {
 	const btn = e.target
-	if (btn.textContent === 'Custom') {
-		removeBtnActive(btnActive, 'btn-primary', 'btn-primary-dark')
-		addBtnStyle(btn, 'btn-primary-outline', 'btn-gray')
-		btnActive = btn
+	removeBtnActive(btnActive, 'btn-primary', 'btn-primary-dark')
+	addBtnStyle(btn, 'btn-primary', 'btn-primary-dark')
+	btnActive = btn
+	amount()
+}
+
+// Set the value to zero in case of NaN
+const setValueToZero = (val) => {
+	val = parseFloat(val)
+	val = (isNaN(val)) ? 0 : val
+	return val
+}
+
+// Calc and set the tips text
+const calcTip = (bill, people, tipPercent) => {
+	const totalPerson = (bill / people * (1 + tipPercent))
+	const tipInd = (bill / people * tipPercent)
+	tipTotalPerson.innerHTML = totalPerson.toFixed(2)
+	tipPerson.innerHTML = tipInd.toFixed(2)
+}
+
+const amount = () => {
+	let inputBillValue = 0
+	let inputPersonValue = 0
+	let inputBtnValue = 0
+	
+	inputBillValue = setValueToZero(inputBill.value)
+	inputPersonValue = setValueToZero(inputPerson.value)
+	inputBtnValue = setValueToZero(inputBtn.value)
+	if (inputPersonValue === 0) {
+		console.log('hey no')
+		return
+	}
+
+	if (inputBtnValue === 0) {
+		let tipPercent = document.querySelector('.btn-primary').value
+		tipPercent = setValueToZero(tipPercent)
+		totalPerson = (inputBillValue / inputPersonValue * (1 + tipPercent))
+		calcTip(inputBillValue, inputPersonValue, tipPercent)
 	} else {
-		removeBtnActive(btnActive, 'btn-primary', 'btn-primary-dark')
-		addBtnStyle(btn, 'btn-primary', 'btn-primary-dark')
-		btnActive = btn
+		inputBtnValue = inputBtnValue / 100
+		calcTip(inputBillValue, inputPersonValue, inputBtnValue)
 	}
 }
 
@@ -45,12 +79,13 @@ window.addEventListener('load', () => {
 		button.addEventListener('click', clickBillButton)
 	}
 
-	inputBill.addEventListener('change', e => {
-		console.log(e)
-	})
+	for (const input of inputs) {
+		input.addEventListener('change', amount)
+		input.addEventListener('focusout', amount)
+	}
 
-	inputPerson.addEventListener('change', () => {
-		console.log(inputPerson.value)
+	inputBtn.addEventListener('click', () => {
+		removeBtnActive(btnActive, 'btn-primary', 'btn-primary-dark')
 	})
 })
 
